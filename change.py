d7c845f22e4ebbf85d390f906ba5c11b072c4945
umangraval <umangraval2000@gmail.com>
2020-07-15 19:44:52 +0530

    firebase change configureed to heroku dyno

--- change.py
+++ change.py
@@ -3,14 +3,14 @@ import fileinput
 import os
 
 try:
-    with fileinput.FileInput('./env/lib/python3.8/site-packages/firebase/__init__.py', inplace=True, backup='.bak') as file:
+    with fileinput.FileInput('/app/.heroku/python/lib/python3.7/site-packages/firebase/__init__.py', inplace=True, backup='.bak') as file:
         for line in file:
             print(line.replace('.async', '.assync'), end='')
 
-    with fileinput.FileInput('./env/lib/python3.8/site-packages/firebase/firebase.py', inplace=True, backup='.bak') as file:
+    with fileinput.FileInput('/app/.heroku/python/lib/python3.7/site-packages/firebase/firebase.py', inplace=True, backup='.bak') as file:
         for line in file:
             print(line.replace('.async', '.assync'), end='')
 
-    os.rename('./env/lib/python3.8/site-packages/firebase/async.py', './env/lib/python3.8/site-packages/firebase/assync.py')
+    os.rename('/app/.heroku/python/lib/python3.7/site-packages/firebase/async.py', '/app/.heroku/python/lib/python3.7/site-packages/firebase/assync.py')
 except:
     print('Already Fixed')
